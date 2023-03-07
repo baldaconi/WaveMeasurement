70273eb718cb78101bc87377c225a76fffcfc345
baldaconi <baldaconi@users.noreply.github.com>
2023-03-07 13:51:30 +0000

    Add files via upload

--- CV_Ball_Tracker.py
+++ CV_Ball_Tracker.py
@@ -15,39 +15,52 @@ import os
 def findCircle(dFrame, targMeanRd, lsc, usc):
 
     hsv = cv.cvtColor(dFrame, cv.COLOR_BGR2HSV)
-    # preparing the mask to overlay
-    mask = cv.inRange(hsv, lsc, usc)
-    # The black region in the mask has the value of 0,
-    # so when multiplied with original image removes all non-orange regions
-    maskedFrame = cv.bitwise_and(dFrame, dFrame, mask=mask)
-    # cv.imshow("Masked", maskedFrame)
-    # cv.waitKey(0)
-
-    grayFrame = cv.cvtColor(maskedFrame, cv.COLOR_BGR2GRAY)
-    blurFrame = cv.GaussianBlur(grayFrame, (7, 7), 0)
-    # cv.imshow("Blur", blurFrame)
-    # cv.waitKey(0)
-    templateCircle = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1, targMeanRd//2,
-                                      param1=25, param2=5,
-                                      minRadius=np.intc(np.round(0.95 * targMeanRd)),
-                                      maxRadius=np.intc(np.round(1.05 * targMeanRd)))
-    if not(templateCircle is None):
-        circle = np.uint16(np.around(templateCircle))
-        circle = circle[0, :]
-    else:
+    if hsv is None:
         circle = None
-    # cv.imshow("Targets found", dFrame)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
+    else:
+        # preparing the mask to overlay
+        mask = cv.inRange(hsv, lsc, usc)
+        # The black region in the mask has the value of 0,
+        # so when multiplied with original image removes all non-orange regions
+        maskedFrame = cv.bitwise_and(dFrame, dFrame, mask=mask)
+        if maskedFrame is None:
+            circle = None
+        else:
+            # cv.imshow("Masked", maskedFrame)
+            # cv.waitKey(0)
+            grayFrame = cv.cvtColor(maskedFrame, cv.COLOR_BGR2GRAY)
+            if grayFrame is None:
+                circle = None
+            else:
+                blurFrame = cv.GaussianBlur(grayFrame, (7, 7), 0)
+                if blurFrame is None:
+                    circle = None
+                else:
+                    # cv.imshow("Blur", blurFrame)
+                    # cv.waitKey(0)
+                    templateCircle = cv.HoughCircles(blurFrame, cv.HOUGH_GRADIENT, 1, targMeanRd//2,
+                                                      param1=25, param2=5,
+                                                      minRadius=np.intc(np.round(0.95 * targMeanRd)),
+                                                      maxRadius=np.intc(np.round(1.05 * targMeanRd)))
+                    if not(templateCircle is None):
+                        circle = np.uint16(np.around(templateCircle))
+                        circle = circle[0, :]
+                    else:
+                        circle = None
+                    # cv.imshow("Targets found", dFrame)
+                    # cv.waitKey(0)
+                    # cv.destroyAllWindows()
     return circle
+
 
 ###################################################################
 numTargets = 20
-showVideoOnScreen = False
+showVideoOnScreen = True
 recordVideoOutput = True
+loadPreSelectedROIs = False
 
 tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'CSRT', 'MOSSE']
-tracker_type = tracker_types[0]
+tracker_type = tracker_types[2]
 
 # tracker = np.empty(numTargets)
 if tracker_type == 'BOOSTING':
@@ -81,12 +94,16 @@ for i in range(1, numTargets):
     if tracker_type == 'MOSSE':
         tracker.append(cv.legacy.TrackerMOSSE_create())
 
-videoFileName = "MVI_0022_converted.mp4"
+videoFileName = "MVI_0013_converted.mp4"
 videoFilePath = "D:\Computer Vision Experiments\Articulated Structure"
 videoFolderCase = "no mooring"
 ballRealSize = 30  # ball size in mm
 fxVideoRsz = 1
 fyVideoRsz = 1
+# for ping pong balls
+# lowerShadeColor = np.array([0, 133, 126])  # darker shades
+# upperShadeColor = np.array([63, 255, 255])  # lighter shades
+# for 20 targets bright targets
 lowerShadeColor = np.array([0, 98, 177])  # darker shades
 upperShadeColor = np.array([180, 255, 255])  # lighter shades
 
@@ -143,7 +160,7 @@ if not ret:
     if not videoCapture.isOpened():
         print("Error opening video stream or file")
 
-else:
+elif loadPreSelectedROIs:
     for i in range(0, numTargets):
         # Select Region of Interest for each target
         dialogTxt = 'Select ROI #{}'.format(i+1)
@@ -207,6 +224,43 @@ else:
         else:
             cv.putText(frame, "Failed", (100, 20), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
             break
+
+    pd.DataFrame(ROI).to_csv('saved_ROI.csv')
+    pd.DataFrame(targetSz).to_csv('saved_TARGET_SZ.csv')
+    pd.DataFrame(targetTemplate).to_csv('saved_TARGETS_BXS.csv')
+else:
+    ROI = pd.read_csv('saved_ROI.csv')
+    targetSz = pd.read_csv('saved_TARGET_SZ.csv')
+    targetTemplate = pd.read_csv('saved_TARGETS_BXS.csv')
+    for i in range(0, numTargets):
+        croppedFrame = frame[int((1./fyVideoRsz)*ROI[i, 1]):int((1./fyVideoRsz)*(ROI[i, 1] + ROI[i, 3])),
+                             int((1./fxVideoRsz)*ROI[i, 0]):int((1./fxVideoRsz)*(ROI[i, 0] + ROI[i, 2]))].copy()
+        ret = tracker[i].init(croppedFrame, targetTemplate[i, :])
+        targetRd[i] = np.average(targetSz[i, 2:4]/2) # radius
+
+        # Find the tracked area in the cropped frame
+        ret, bbox = tracker[i].update(croppedFrame)
+        if ret:
+            bboxFrame = croppedFrame[int(bbox[1]):int(bbox[1]+bbox[3]), int(bbox[0]):int(bbox[0]+bbox[2])]
+            # Find a circle with the defined radius within the tracked area
+            circles = findCircle(bboxFrame, targetRd[i], lowerShadeColor, upperShadeColor)
+            if not(circles is None):
+                # Recalculate the target radius
+                targetRd[i] = np.average([targetRd[i], circles[0, 2]])  # radius
+
+                p1 = (int(bbox[0] + ROI[i, 0]), int(bbox[1] + ROI[i, 1]))
+                p2 = (int(p1[0] + bbox[2]), int(p1[1] + bbox[3]))
+                bboxPosition[i, kFrame, :] = bbox
+                # circlePosition[i, kFrame, :] = circles[0, :]
+                # Circles positions are relative to the bounding boxes
+                # They need to be added to bbox and ROI top left corners so they are in the global axes
+                circlePosition[i, kFrame, 0] = int(round((circles[0, 0] + targetRd[i] + targetSz[i, 0] - bbox[0])/2))
+                circlePosition[i, kFrame, 1] = int(round((circles[0, 1] + targetRd[i] + targetSz[i, 1] - bbox[1])/2))
+                circlePosition[i, kFrame, 2] = int(round(targetRd[i]))
+                cv.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+                cv.circle(frame, (int(circlePosition[i, kFrame, 0] + p1[0]), int(circlePosition[i, kFrame, 1] + p1[1])),
+                          int(circlePosition[i, kFrame, 2]), (255, 0, 255), 2)
+
     if len(targetRd) == numTargets:
         while True:
             # # Reading the video frame
@@ -223,9 +277,9 @@ else:
                 croppedFrame = originalFrame[
                                int((1. / fyVideoRsz) * ROI[i, 1]):int((1. / fyVideoRsz) * (ROI[i, 1] + ROI[i, 3])),
                                int((1. / fxVideoRsz) * ROI[i, 0]):int((1. / fxVideoRsz) * (ROI[i, 0] + ROI[i, 2]))].copy()
-                ret = tracker[i].init(croppedFrame, bboxPosition[i, kFrame-1, :])
-                if not ret:
-                    ret = tracker[i].init(croppedFrame, targetTemplate[i, :] // 2)
+                # ret = tracker[i].init(croppedFrame, bboxPosition[i, kFrame-1, :])
+                # if not ret:
+                #    ret = tracker[i].init(croppedFrame, targetTemplate[i, :] // 2)
                 # Check if performance improves by doing the .init with the bbox found in the previous step
 
                 # Find the tracked area in the cropped frame
@@ -233,10 +287,21 @@ else:
                 if ret:
                     p1 = (int(bbox[0] + ROI[i, 0]), int(bbox[1] + ROI[i, 1]))
                     p2 = (int(p1[0] + bbox[2]), int(p1[1] + bbox[3]))
-                    bboxPosition[i, kFrame, :] = bbox
-                    circlePosition[i, kFrame, 0] = int(round(circlePosition[i, 0, 0] + p1[0]))
-                    circlePosition[i, kFrame, 1] = int(round(circlePosition[i, 0, 1] + p1[1]))
-                    circlePosition[i, kFrame, 2] = int(round(targetRd[i]))
+                    bboxFrame = croppedFrame[int(bbox[1]):int(bbox[1] + bbox[3]), int(bbox[0]):int(bbox[0] + bbox[2])]
+                    circles = findCircle(bboxFrame, targetRd[i], lowerShadeColor, upperShadeColor)
+                    if circles is None:
+                        circlePosition[i, kFrame, 0] = int(-99999)#int(circlePosition[i, kFrame-1, 0])
+                        circlePosition[i, kFrame, 1] = int(-99999)#int(circlePosition[i, kFrame-1, 1])
+                        circlePosition[i, kFrame, 2] = int(round(targetRd[i]))
+                    else:
+                        bboxPosition[i, kFrame, :] = bbox
+                        circlePosition[i, kFrame, 0] = int(round(circles[0, 0] + p1[0]))
+                        circlePosition[i, kFrame, 1] = int(round(circles[0, 1] + p1[1]))
+                        circlePosition[i, kFrame, 2] = int(round(targetRd[i]))
+
+                    # circlePosition[i, kFrame, 0] = int(round(circlePosition[i, 0, 0] + p1[0]))
+                    # circlePosition[i, kFrame, 1] = int(round(circlePosition[i, 0, 1] + p1[1]))
+                    # circlePosition[i, kFrame, 2] = int(round(targetRd[i]))
                     if showVideoOnScreen or recordVideoOutput:
                         cv.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
                         cv.circle(frame, (int(circlePosition[i, kFrame, 0]),
